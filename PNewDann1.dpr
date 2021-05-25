a8932a23e7e7560921facc4f1a0caec2719dc680
Jens01 <jens-biermann@gmx.net>
2021-05-25 15:10:12 +0200

    Add files via upload

--- PNewDann1.dpr
+++ PNewDann1.dpr
@@ -11,12 +11,7 @@ uses
   NewDann.Data in 'NewDann.Data.pas',
   NewDann.frm.Graph in 'NewDann.frm.Graph.pas' {frmGraph: TFrame},
   NiceChart in 'NiceChart\NiceChart.pas',
-  BSplines in 'NiceChart\BSplines.pas',
-  DelphiCL in 'OpenCL\DelphiCL.pas',
-  CL_Platform in 'OpenCL\CL_Platform.pas',
-  CL_GL in 'OpenCL\CL_GL.pas',
-  CL in 'OpenCL\CL.pas',
-  NewDann.OpenCL in 'NewDann.OpenCL.pas';
+  BSplines in 'NiceChart\BSplines.pas';
 
 {$R *.res}
 
